c76ba1312594a207e6b1c2335d6276faad09acec
cfigueirasvalverde <cfigueirasvalverde@nautilus13.ad.danielcastelao.org>
2020-02-12 09:02:45 +0100

    Boletin_19

---
+++ boletin19/Boletin_19/src/boletin_19/Boletin_19.java
@@ -0,0 +1,17 @@
+package boletin_19;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin_19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Futbol f = new Futbol();
+        f.cuestionario();
+    }
+    
+}
